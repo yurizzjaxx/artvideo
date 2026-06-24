accd14d4df86e87b625624416ec0996d6064da2e
yuriZzJaxx <132253210+yurizzjaxx@users.noreply.github.com>
2026-06-24 00:21:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var vi = document.createElement("video");
+vi.src = "hutao.mp4"
+vi.id = "_vi"
+vi.autoplay = true
+vi.width = screen.width
+document.getElementById("video").appendChild(vi)
+
+document.addEventListener("keydown", (event) => {
+  switch (event.keyCode) {
+    case 415:
+      vi.play()
+    break;
+    case 19:
+      vi.pause()
+    break;
+    case 417:
+      vi.currentTime = Math.min(vi.duration, vi.currentTime + 5)
+    break;
+    case 412:
+      vi.currentTime = Math.max(0, vi.currentTime - 5)
+    break;
+  }
+})
